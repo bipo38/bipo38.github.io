0c5280d3a9146d5648e4f4ab22b5b2e17fc61bba
bipo38 <bipoben7@gmail.com>
2022-12-15 19:06:57 +0100

    music

--- start_page/js/time.js
+++ start_page/js/time.js
@@ -1,19 +1,23 @@
 let state = false;
-const snow = document.querySelector('.not-snow');
+const snow = document.querySelector(".not-snow");
 
-const hours = document.querySelector('.hour');
-const min = document.querySelector('.minutes');
-const sec = document.querySelector('.seconds');
+const audio = document.querySelector("audio");
+const btnPlay = document.querySelector(".play");
+let stateAudio = false;
 
-const day = document.querySelector('.day');
-const month = document.querySelector('.month');
-const year = document.querySelector('.year');
+const hours = document.querySelector(".hour");
+const min = document.querySelector(".minutes");
+const sec = document.querySelector(".seconds");
+
+const day = document.querySelector(".day");
+const month = document.querySelector(".month");
+const year = document.querySelector(".year");
 
 function getTime() {
   const date = new Date();
-  const hour = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const seconds = date.getSeconds().toString().padStart(2, '0');
+  const hour = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
   hours.innerText = `${hour} `;
   min.innerText = `${minutes} `;
   sec.innerText = `${seconds} `;
@@ -39,17 +43,30 @@ function init() {
 init();
 
 //Autofocus for firefox
-$('.search').focus();
+$(".search").focus();
 
 //Snow
 
 function toggleSnow() {
   state = !state;
   if (state) {
-    snow.classList.remove('not-snow');
-    snow.classList.add('snowflakes');
+    snow.classList.remove("not-snow");
+    snow.classList.add("snowflakes");
     return;
   }
-  snow.classList.remove('snowflakes');
-  snow.classList.add('not-snow');
+  snow.classList.remove("snowflakes");
+  snow.classList.add("not-snow");
+}
+
+//Audio
+
+function toggleAudio() {
+  stateAudio = !stateAudio;
+  if (stateAudio) {
+    audio.play();
+    btnPlay.classList.add("rotate");
+    return;
+  }
+  btnPlay.classList.remove("rotate");
+  audio.pause();
 }
