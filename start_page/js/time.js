c8d3da9b0f89077b6d6aea7ebe9123ef6aa1af07
bipo38 <bipoben7@gmail.com>
2022-12-15 23:00:16 +0100

    Refactorv2

--- start_page/js/time.js
+++ start_page/js/time.js
@@ -1,23 +1,25 @@
 let state = false;
-const snow = document.querySelector(".not-snow");
+const snow = document.querySelector('.not-snow');
 
-const audio = document.querySelector("audio");
-const btnPlay = document.querySelector(".play");
+const audio = document.querySelector('audio');
+const btnPlay = document.querySelector('.play');
 let stateAudio = false;
 
-const hours = document.querySelector(".hour");
-const min = document.querySelector(".minutes");
-const sec = document.querySelector(".seconds");
+const hours = document.querySelector('.hour');
+const min = document.querySelector('.minutes');
+const sec = document.querySelector('.seconds');
 
-const day = document.querySelector(".day");
-const month = document.querySelector(".month");
-const year = document.querySelector(".year");
+const logo = document.querySelector('.logo');
+
+const day = document.querySelector('.day');
+const month = document.querySelector('.month');
+const year = document.querySelector('.year');
 
 function getTime() {
   const date = new Date();
-  const hour = date.getHours().toString().padStart(2, "0");
-  const minutes = date.getMinutes().toString().padStart(2, "0");
-  const seconds = date.getSeconds().toString().padStart(2, "0");
+  const hour = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
   hours.innerText = `${hour} `;
   min.innerText = `${minutes} `;
   sec.innerText = `${seconds} `;
@@ -42,20 +44,19 @@ function init() {
 
 init();
 
-//Autofocus for firefox
-$(".search").focus();
-
 //Snow
 
 function toggleSnow() {
   state = !state;
   if (state) {
-    snow.classList.remove("not-snow");
-    snow.classList.add("snowflakes");
+    snow.classList.remove('not-snow');
+    snow.classList.add('snowflakes');
+    logo.src = './assets/img/bongo.gif';
     return;
   }
-  snow.classList.remove("snowflakes");
-  snow.classList.add("not-snow");
+  logo.src = './assets/img/bongost.gif';
+  snow.classList.remove('snowflakes');
+  snow.classList.add('not-snow');
 }
 
 //Audio
@@ -64,9 +65,9 @@ function toggleAudio() {
   stateAudio = !stateAudio;
   if (stateAudio) {
     audio.play();
-    btnPlay.classList.add("rotate");
+    btnPlay.classList.add('rotate');
     return;
   }
-  btnPlay.classList.remove("rotate");
+  btnPlay.classList.remove('rotate');
   audio.pause();
 }
